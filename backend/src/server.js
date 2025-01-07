c1a0783616bb234afaf2c3fb40741fd80e3dac66
ronaldkinyuru <ronaldkinyuru>
2025-01-07 13:41:41 +0300

    registration succesful

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
+
+dotenv.config();
 
 const app = express();
 
 //middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 //routes
 app.use('/api/auth', authRoutes);
