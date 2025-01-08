4c3587989d58825a6fe5a607f425cb7463735b01
Ronald <ronaldkinyuru@GMAIL.COM>
2025-01-08 19:32:14 +0300

    Update property.js

--- backend/src/routes/property.js
+++ backend/src/routes/property.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { addProperty, getProperties, updateProperty, deleteProperty,getPropertyById } = require('../controllers/propertiesController');
+const { addProperty, getProperties, getPropertyById, updateProperty, deleteProperty } = require('../controllers/property');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/properties', authMiddleware, addProperty);
-router.get('/properties', getProperties);
-router.get('/properties/:id', getPropertyById);
-router.put('/properties/:id', authMiddleware, updateProperty)
-router.delete('/properties/:id', authMiddleware, deleteProperty)
+router.post('/properties', authMiddleware, addProperty); // Add property
+router.get('/properties', getProperties); // Get all properties
+router.get('/properties/:id', getPropertyById); // Get property by ID
+router.put('/properties/:id', authMiddleware, updateProperty); // Update property
+router.delete('/properties/:id', authMiddleware, deleteProperty); // Delete property
 
 module.exports = router;
